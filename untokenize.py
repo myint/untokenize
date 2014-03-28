5baf4a09154368b40479d86db09d30ad9e455368
Steven Myint <git@stevenmyint.com>
2014-03-28 06:29:30 -0700

    Format

--- untokenize.py
+++ untokenize.py
@@ -40,9 +40,9 @@ def untokenize(tokens):
     """Return source code based on tokens.
 
     This is like tokenize.untokenize(), but it preserves spacing between
-    tokens. So if the original soure code had multiple spaces between some
-    tokens or if escaped newlines were used, those things will be reflected
-    by untokenize().
+    tokens. So if the original soure code had multiple spaces between
+    some tokens or if escaped newlines were used, those things will be
+    reflected by untokenize().
 
     """
     text = ''
